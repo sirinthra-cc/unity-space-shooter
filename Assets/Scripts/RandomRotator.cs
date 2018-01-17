d8c4faaa9659ad9c0b2d22290982794b3fec4a29
Sirinthra Chantharaj <sirinthra.cc@gmail.com>
2018-01-17 21:34:16 +0700

    Destroy asteroid by contact

---
+++ Assets/Scripts/RandomRotator.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomRotator : MonoBehaviour {
+
+    public float tumble;
+
+    private void Start()
+    {
+        Rigidbody asteroid = gameObject.GetComponent<Rigidbody>();
+        asteroid.angularVelocity = Random.insideUnitSphere * tumble;
+    }
+}
