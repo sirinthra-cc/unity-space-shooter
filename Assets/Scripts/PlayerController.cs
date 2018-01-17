2e6fe5d693461c583296641a66d688f86f984d0c
Sirinthra Chantharaj <sirinthra.cc@gmail.com>
2018-01-17 17:06:55 +0700

    Add player controller

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Collections;
+
+[System.Serializable]
+public class Boundary
+{
+    public float xMin, xMax, zMin, zMax;
+}
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+    public float tilt;
+    public Boundary boundary;
+
+    private void FixedUpdate() {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+        Rigidbody shooter = gameObject.GetComponent<Rigidbody>();
+        shooter.velocity = movement * speed;
+        shooter.position = new Vector3(
+            Mathf.Clamp(shooter.position.x, boundary.xMin, boundary.xMax),
+            0.0f,
+            Mathf.Clamp(shooter.position.z, boundary.zMin, boundary.zMax)
+        );
+        shooter.rotation = Quaternion.Euler(0.0f, 0.0f, shooter.velocity.x * -tilt);
+    }
+}
