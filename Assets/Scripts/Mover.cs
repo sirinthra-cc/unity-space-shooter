243dfb46b72414871b59c39cdff7de5a47839d5b
Sirinthra Chantharaj <sirinthra.cc@gmail.com>
2018-01-17 17:29:04 +0700

    Create shots

---
+++ Assets/Scripts/Mover.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mover : MonoBehaviour {
+
+    public float speed;
+
+    private void Start()
+    {
+        Rigidbody laser = gameObject.GetComponent<Rigidbody>();
+        laser.velocity = transform.forward * speed;
+    }
+}
